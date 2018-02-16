b352ae00f4a2306007499a58ac90d9c11dc655a1
Alex Bertram <alex@bedatadriven.com>
2018-02-16 23:27:34 +0100

    Added presentation on NLP

--- packages.R
+++ packages.R
@@ -4,7 +4,7 @@
 installed <- installed.packages()[,"Package"]
 
 # Install packages from CRAN
-cran_packages <- c("ggplot2", "rJava", "tm", "devtools", "httr")
+cran_packages <- c("ggplot2", "rJava", "tm", "devtools", "httr", "NLP", "openNLP")
 uninstalled <- cran_packages[!(cran_packages %in% installed)]
 
 if(length(uninstalled) > 0) {
@@ -16,3 +16,10 @@ if(!("pdfbox" %in% installed)) {
   devtools::install_github("hrbrmstr/pdfboxjars")
   devtools::install_github("hrbrmstr/pdfbox")
 }
+
+# Install open NLP models
+nlp_models <- c("openNLPmodels.nl", "openNLPmodels.en")
+if(any(!(nlp_models %in% installed))) {
+  install.packages(nlp_models, repos = "http://datacube.wu.ac.at/", type = "source") 
+}
+
