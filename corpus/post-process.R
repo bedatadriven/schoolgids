c2d0b1576f17a24c30fb02daa9cd9893ff81f9ed
Alex Bertram <alex@bedatadriven.com>
2018-02-19 23:12:06 +0100

    Added post-process script to clean up text

---
+++ corpus/post-process.R
@@ -0,0 +1,39 @@
+
+
+# Post-process extract text.
+# We want to remove stray text that will interfere with the natural flow
+# of paragraphs and inhibit natural language processing.
+
+inputs <- list.files("pdf", "\\.txt$")
+for(input in inputs) {
+  lines <- readLines(file.path("pdf", input), encoding = "UTF-8")
+
+  # Remove leading and trailing spaces
+  lines <- gsub(x = lines, pattern = "^\\s+", replacement = "")
+  lines <- gsub(x = lines, pattern = "\\s+$", replacement = "")
+
+  # Find repeated lines: these are headers and footers
+  freqs <- table(lines)
+  headers <- names(freqs[freqs > 10])
+
+  # Remove headers
+  header_lines <-  (lines %in% headers)
+  
+  # remove page numbers
+  page_numbers <- grepl(lines, pattern = "^pagina \\d", ignore.case = TRUE) |
+    grepl(lines, pattern = "^\\d+$")
+  
+  # Exclude lines with low text content
+  # This includes tables of contents with ".........34" and
+  # tables that frog does not understand
+  count_alpha <- nchar(gsub(lines, pattern = "[^[:alpha:]]", replacement = ""))
+  count_chars <- nchar(lines, allowNA = TRUE)
+  alpha_prop <- count_alpha / count_chars
+  alpha_prop[is.na(alpha_prop)] <- 1
+
+  blank_lines <- (lines == "")
+  
+  lines <- lines[ blank_lines | (!header_lines & !page_numbers & alpha_prop > 0.5) ]
+
+  writeLines(lines, con = file.path("pdf", sprintf("%s.post", input)))
+}
